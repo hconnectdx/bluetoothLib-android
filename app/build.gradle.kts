e8c32d525221c311c6c3ab2eb5f4c8ac0b11f8f2
MinwooHconnect <kmwdev@hconnect.co.kr>
2024-06-20 15:23:06 +0900

    compose 기본 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    println("")
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,6 +35,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    // compose 추가
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
@@ -44,7 +52,17 @@ dependencies {
     implementation(libs.material)
     implementation(project(":bluetoothlib"))
 
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ bluetoothlib/src/main/java/kr/co/hconnect/bluetoothlib/Testmy.kt
@@ -0,0 +1,10 @@
+package kr.co.hconnect.bluetoothlib
+
+import android.util.Log
+import android.widget.Toast
+
+class Testmy {
+    fun test() {
+        Log.d("Testmy", "This is a debug log message")
+    }
+}
